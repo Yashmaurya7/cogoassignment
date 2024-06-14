d4df04edf6b00e301157caf8c15658a832d335a8
Yashmaurya7 <mauryayash2011@gmail.com>
2024-06-14 15:31:52 +0530

    Update create_tables.py

--- config_manager/create_tables.py
+++ config_manager/create_tables.py
@@ -8,14 +8,10 @@
 # import sys
 # sys.path.append('../')
 import sys
-import os
-
-# Add the root directory of the project to the Python path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# config_manager/create_tables.py
 from sqlalchemy import create_engine
-from config_manager.database import DATABASE_URL, metadata
 from config_manager.models.configuration import configuration
-
+from config_manager.database import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 metadata.create_all(engine)
